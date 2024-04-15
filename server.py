fbc7e8afa85542479169009387f649ac66e38b43
Lauren Osborne <losborne@sfgiants.com>
2024-04-14 23:38:34 -0400

    Added skeleton functions for server

--- server.py
+++ server.py
@@ -4,16 +4,30 @@ import json
 
 app = Flask(__name__)
 global data
-
+'''
 with open('data.json', 'r') as file:
     data = json.load(file)
-currentId = len(data) + 1 
+'''
+data = None
 
 # ROUTES
 
 @app.route('/')
-def hello_world():
+def welcome():
    return render_template('welcome.html', data=data)   
+
+@app.route('/upper_body')
+def upper_body():
+   return render_template('upper_body_home.html') 
+
+@app.route('/lower_body')
+def lower_body():
+   return render_template('lower_body_home.html') 
+
+@app.route('/learn/<string:lesson_id>', methods=['GET', 'POST'])
+def learn(lesson_id):
+   global data
+   return render_template('exercise.html', item=item) 
 
 if __name__ == '__main__':
    app.run(debug = True)
