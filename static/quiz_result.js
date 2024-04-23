26e8d99dd73ee4877c9ec21760c29cb1dbccb767
hhuang <hwhuang01@gmail.com>
2024-04-23 15:22:16 -0400

    quiz ui changes for color

--- static/quiz_result.js
+++ static/quiz_result.js
@@ -4,9 +4,11 @@ function display_result_text(c, count) {
   $group.append($("<div>").text("You scored " + c + " out of " + count));
   if (parseInt(c) < 2) {
     $group.append(
-      $("<div>").text(
-        "If you think you can do better, review the material and then try again!"
-      )
+      $("<div>")
+        .text(
+          "If you think you can do better, review the material and then try again!"
+        )
+        .addClass("gray-font")
     );
   } else {
     $group.append($("<div>").text("Great job!"));
@@ -16,16 +18,27 @@ function display_result_text(c, count) {
 
 function display_previous_results(prev_results) {
   let $prev_results_table = $("#prev_results_table");
-  Object.keys(prev_results).forEach(function(attempt) {
-    let $row = $("<div>").addClass("row")
-    $row.append($("<div class='col-4'>" + attempt + "</div><div class='col-4'>" + prev_results[attempt]["correct"] + "</div><div class='col-4'>" + prev_results[attempt]["percent"] + "%" + "</div>"))
-    $prev_results_table.append($row)
-})
+  Object.keys(prev_results).forEach(function (attempt) {
+    let $row = $("<div>").addClass("row");
+    $row.append(
+      $(
+        "<div class='col-4'>" +
+          attempt +
+          "</div><div class='col-4'>" +
+          prev_results[attempt]["correct"] +
+          "</div><div class='col-4'>" +
+          prev_results[attempt]["percent"] +
+          "%" +
+          "</div>"
+      )
+    );
+    $prev_results_table.append($row);
+  });
 }
 
 $(document).ready(function () {
   display_result_text(correct_count, q_count);
-  display_previous_results(prev_results)
+  display_previous_results(prev_results);
   $("#home-button").click(function () {
     window.location.href = "/";
   });
